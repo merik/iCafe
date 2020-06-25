1117de781e9631eb3502ec8701a8004a74007017
Erik Mai <merik312@gmail.com>
2020-06-25 13:25:49 +1000

    Initial Commit

---
+++ iCafe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iCafe
+//
+//  Created by Erik Mai on 25/6/20.
+//  Copyright © 2020 Erik Mai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
